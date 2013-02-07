3544a829ab278802104e8a8dae2a0fe9f0da64bd
unknown <cgoldberg@CRAIG-T.ne.virtmed.com>
2013-02-07 17:03:14 -0500

    loading defect 5 for DEV-15997

---
+++ src/chabot/utils/outofmemory/Defect_05.java
@@ -0,0 +1,43 @@
+package chabot.utils.outofmemory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * DEV-15997
+ * https://jira/browse/DEV-15997
+ */
+
+public class Defect_05 extends Signature {
+
+	final String COLUMNDATA = "CrossTabColumnData";
+	final String COLUMNROW = "CrossTabRowData";	
+		
+	public Defect_05() {		
+		
+		BadClass class_1 = new BadClass();		
+		class_1.setName(COLUMNDATA);
+		class_1.setNumber(200);
+		
+		BadClass class_2 = new BadClass();		
+		class_2.setName(COLUMNROW);
+		class_2.setNumber(200);		
+		
+		Map<String, BadClass> classList = new HashMap<String, BadClass>();
+		classList.put(class_1.getName(), class_1);
+		classList.put(class_2.getName(), class_2);		
+				
+		setName("DEV-15997");
+		setDescription("Fill this in later.");
+		setFixVersion("7.x Triage");
+		setClassList(classList);				
+	}
+	
+	public boolean evaluate(AnalyzedHeap analyzedHeap) {		
+		if ((analyzedHeap.getNumber(COLUMNDATA) >= classList.get(COLUMNDATA).getNumber() || 
+			analyzedHeap.getNumber(COLUMNROW) >= classList.get(COLUMNROW).getNumber())) {			
+			return true;
+		}		
+		return false;
+	}
+}
