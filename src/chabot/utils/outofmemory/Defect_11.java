2485734d660734bd8a1b139e3d36563e38868bf6
cthonis <cthonis@patientkeeper.com>
2013-03-06 13:25:26 -0500

    Minor tweaks and formatting updates made to defect 11

--- src/chabot/utils/outofmemory/Defect_11.java
+++ src/chabot/utils/outofmemory/Defect_11.java
@@ -10,34 +10,34 @@ import java.util.Map;
 
 public class Defect_11 extends Signature {
 
-	final String IMPL = "SessionImpl";
+	final String IMPL = "net.sf.hibernate.impl.SessionImpl";
 	final String VALCALLBACK = "Val$callback";
 	final String ORDERDEF = "GetOrderDefinitionList";
-	final String CDSCODE = "CdsCode";
+	final String CDSCODE = "com.patientkeeper.comaco.cds.CdsCode";
 	
 	public Defect_11() {		
 		
 		BadClass class_1 = new BadClass();		
 		class_1.setName(IMPL);
-		class_1.setQuestion("Is there an instance of net.sf.hibernate.impl.SessionImpl that is over 100MB?");
+		class_1.setQuestion("Is there an instance of <span class=\"class\">" + IMPL + "</span> that is over 100MB?");
 		
 		BadClass class_2 = new BadClass();		
 		class_2.setName(VALCALLBACK);
-		class_2.setQuestion("Does this instance contain a [this$0, val$callback] class?");
+		class_2.setQuestion("Does this instance contain a <span class=\"class\">" + VALCALLBACK + "</span> class?");
 		
 		BadClass class_3= new BadClass();		
 		class_3.setName(ORDERDEF);
-		class_3.setQuestion("Does the [this$0, val$callback] class reference GetOrderDefinition?");
+		class_3.setQuestion("Does the <span class=\"class\">" + VALCALLBACK + "</span> class reference <span class=\"class\">" + ORDERDEF + "</span>?");
 		
 		BadClass class_4= new BadClass();		
-		class_3.setName(CDSCODE);
-		class_3.setQuestion("Does com.patientkeeper.comaco.cds.CdsCode contain more than 100 thousand objects?");
+		class_4.setName(CDSCODE);
+		class_4.setQuestion("Does <span class=\"class\">" + CDSCODE + "</span> contain more than 100 thousand objects?");
 		
 		Map<String, BadClass> classList = new HashMap<String, BadClass>();
 		classList.put(class_1.getName(), class_1);
 		classList.put(class_2.getName(), class_2);	
 		classList.put(class_3.getName(), class_3);
-		classList.put(class_3.getName(), class_3);
+		classList.put(class_4.getName(), class_4);
 		
 		setName("DEV-31450");
 		setDescription("Fill this in later.");
