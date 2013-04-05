d26c89d1fddc70fe0dcabd5927e868aa3a70d387
mikechabot <mchabot21@gmail.com>
2013-04-05 17:42:08 -0400

    fixing up a merge issue on my side, possibly committing some fo the changes for changing to LinkedHashMap

--- src/com/patientkeeper/outofmemory/Defect_08.java
+++ src/com/patientkeeper/outofmemory/Defect_08.java
@@ -4,7 +4,11 @@
 
 package com.patientkeeper.outofmemory;
 
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.patientkeeper.tools.ToHTML;
 
 /*
  * DEV-39170
@@ -31,7 +35,7 @@ public class Defect_08 extends Signature {
 		class_3.setName(PROBLEMLIST);
 		class_3.setQuestion("Is <span class=\"class\">" + PROBLEMLIST + "</span> the first or second largest class within the largest thread?");
 		
-		LinkedHashMap<String, BadClass> classList = new LinkedHashMap<String, BadClass>();
+		Map<String, BadClass> classList = new HashMap<String, BadClass>();
 		classList.put(class_1.getName(), class_1);
 		classList.put(class_2.getName(), class_2);
 		classList.put(class_3.getName(), class_3);
@@ -41,6 +45,16 @@ public class Defect_08 extends Signature {
 		setBlurb("Review the Dominator Tree to identify the items below:");
 		setFixVersion("7.6.6");
 		setClassList(classList);
+		
+		List<String> instructions = new ArrayList<String>(0);
+		instructions.add("Open the dominator tree and sort by percent descending.");
+		instructions.add("Determine if either of the below class syntaxes are the largest class and update the <em>largest threads</em> questions");
+		instructions.add("A HH thread like <strong>) java.lang.Thread @<address> Sync08:<user_nm>:2:<sessionlog_id>:Submission[PATIENTID-<pat_id>]</strong>");
+		instructions.add("A web thread (or ThreadWithAttributes) like <strong>org.apache.tomcat.util.threads.ThreadWithAttributes @<address> http-0.0.0.0-31140-Processor6</strong>");
+		instructions.add("Expand this thread, determine if <strong>com.patientkeeper.datamodel.ProblemList</strong> is the first or second largest class, and update the <em>ProblemList</em> question accordingly.");
+		String ordered = ToHTML.getOrderedList(instructions);
+
+		setInstructions(ordered);
 		
 	}//end Defect
 	
